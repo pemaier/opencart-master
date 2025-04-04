38790694d6a91a758f9c2555a56378a88b18eb9b
pemaier <pemaier@users.noreply.github.com>
2025-04-04 23:34:56 +0300

    Update country.php - remove double entry

--- upload/admin/language/en-gb/ssr/country.php
+++ upload/admin/language/en-gb/ssr/country.php
@@ -1,4 +1,3 @@
 <?php
 $_['text_country'] = 'You have rendered %s to %s of %s countries';
-$_['text_country'] = 'You have rendered %s to %s of %s countries';
 $_['text_success'] = 'Success: You have modified countries!';
